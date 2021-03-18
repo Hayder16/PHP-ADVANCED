c2c769c458921b11d204565f608e53dac23de9d5
Hayder Abidi <hayder.abidi@student.rocva.nl>
2021-03-18 12:26:35 +0100

    Taak 1

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$Piet['Voornaam'] = 'Piet';
+$Mo['Voornaam'] = 'Mo';
+$Els['Voornaam'] = 'Els';
+
+$Piet['Achternaam'] = 'Yours';
+$Mo['Achternaam'] = 'Nour';
+$Els['Achternaam'] = 'Haren';
+
+$Piet['Leeftijd'] = 17;
+$Mo['Leeftijd'] = 18;
+$Els['Leeftijd'] = 16;
+
+$Piet['Klas'] = 9;
+$Mo['Klas'] = 9;
+$ELs['Klas'] = 8;
+
+print "<pre>";
+print_r($Mo) . "<br>"; 
+print_r($Els) . "<br>";
+print_r($Piet) . "<br>";
+print "</pre>";
+
+
+
+
+?>
+
+</body>
+</html>
