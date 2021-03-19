5e590e8f7f4bf814af6b304d56df9c83664fb69c
Hayder Abidi <hayder.abidi@student.rocva.nl>
2021-03-19 11:24:52 +0100

    Loops-foreach-deel2

---
+++ 2-Loops/taak04/Loops-ForEach-Deel2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <form>
+        <label for="Klas">Kies een je klas:</label>
+        <select name="Klas" id="Klass">
+<?php
+
+    define('MAX_OPTIONS', 6);
+
+    $klassen = 
+    array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
+
+    foreach($klassen as $klas){
+        echo "<ul><li><option>  $klas  </option></li></ul>";
+    }
+
+?>
+
+        </select>
+    </form>
+
+    <button type="Button">Send</button>
+
+</body>
+</html>
