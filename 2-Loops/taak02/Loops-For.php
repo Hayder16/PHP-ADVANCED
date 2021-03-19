dd420c7c66414a89c400af65b90a591fbabd79c9
Hayder Abidi <hayder.abidi@student.rocva.nl>
2021-03-19 10:36:16 +0100

    Loops-For

---
+++ 2-Loops/taak02/Loops-For.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+for($x = 0; $x < 18; $x++){
+    echo "<ul><li> Ik ben $x jaar, dus ik mag nog niet stemmen </li></ul>";
+}
+
+echo "<ul><li> Ik ben 18 jaar oud dus ik heb stemrecht! </li></ul>";
+
+?>
+
+</body>
+</html>
