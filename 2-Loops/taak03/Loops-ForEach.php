2a73a0ff950dd4012609f164011c5fa0d72ebd4c
Hayder Abidi <hayder.abidi@student.rocva.nl>
2021-03-19 10:49:55 +0100

    Loops-ForEach

---
+++ 2-Loops/taak03/Loops-ForEach.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$dagen = 
+array("Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag", "Zondag");
+
+foreach($dagen as $week){
+    echo "<ul><li> $week </li></ul>";
+}
+
+?>
+
+</body>
+</html>
