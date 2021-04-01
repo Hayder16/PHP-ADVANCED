f8d6a46dd96bbb1b094ea91d9cddc1810e94b158
Hayder Abidi <hayder.abidi@student.rocva.nl>
2021-04-01 14:41:43 +0100

    functions 1

---
+++ 4-Functions/taak01/functies-1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+    function schrijfKleur(){
+        echo "<p style='color:red'>Deze tekst heeft de kleur rood</p>";
+        echo "<p style='color:green'>Deze tekst heeft de kleur groen</p>";
+        echo "<p style='color:blue'>Deze tekst heeft de kleur blauw</p>";
+        echo "<p style='color:yellow'>Deze tekst heeft de kleur geel</p>";
+    }
+    schrijfKleur();
+    
+    ?>
+</body>
+</html>
